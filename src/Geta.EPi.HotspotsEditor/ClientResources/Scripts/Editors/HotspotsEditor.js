829d85d0a8d473263d051f116209b903d326e90a
Magnus Ellinge <magnus.ellinge@consid.se>
2023-11-15 00:41:24 +0100

    Allow to modify allowedtypes / roots, remove dependency on commerce Localization

--- src/Geta.EPi.HotspotsEditor/ClientResources/Scripts/Editors/HotspotsEditor.js
+++ src/Geta.EPi.HotspotsEditor/ClientResources/Scripts/Editors/HotspotsEditor.js
@@ -1,162 +1,153 @@
 ﻿define([
     "dojo/_base/declare",
+    "dojo/_base/lang",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
     "epi-cms/_ContentContextMixin",
     "epi-cms/widget/ContentSelector",
+    "epi/i18n!epi/cms/nls/geta.clientresources.hotspotseditor",
+    "epi/i18n!epi/cms/nls/shell.cms.externallinks.details",
     'dojo/text!./Templates/HotspotsEditor.html'
-], function (declare, _WidgetBase, _TemplatedMixin, _ContentContextMixin, ContentSelector, template) {
-
+], function (declare, lang, _WidgetBase, _TemplatedMixin, _ContentContextMixin, ContentSelector, localizations, externallinksResources, template) {
     return declare("hotspots/editors/HotspotsEditor",
         [_WidgetBase, _TemplatedMixin, _ContentContextMixin], {
-            templateString: template,
+        templateString: template,
 
-            //********************************************************************************
-            //*PROTOTYPE/PUBLIC FUNCTIONS*****************************************************
-            //********************************************************************************
+        _resources: lang.mixin(localizations, externallinksResources),
 
-            postCreate: function () {
+        //********************************************************************************
+        //*PROTOTYPE/PUBLIC FUNCTIONS*****************************************************
+        //********************************************************************************
 
-                this.set('value', this.value);
+        postCreate: function () {
+            this.set('value', this.value);
 
-                this.activeHotSpot = undefined;
+            this.activeHotSpot = undefined;
 
-                this.HotSpotsEditor = new Oxx.HotSpotsEditor(this.domNode);
-                this.HotSpotsEditor.setValue(this.value);
+            this.HotSpotsEditor = new Oxx.HotSpotsEditor(this.domNode);
+            this.HotSpotsEditor.setValue(this.value);
 
+            this._attachEventListeners();
+        },
 
-                this._attachEventListeners();
+        //********************************************************************************
+        //*PRIVATE OBJECT METHODS ********************************************************
+        //********************************************************************************
 
-            },
+        /**
+        * Attach events here
+        *
+        * @private
+        */
+        _attachEventListeners: function () {
+            jQuery(this.HotSpotsEditor).on('change', jQuery.proxy(this._onChange, this));
+            jQuery(this.HotSpotsEditor).on('select', jQuery.proxy(this._onHotSpotSelected, this));
+            jQuery(this.HotSpotsEditor).on('deselect', jQuery.proxy(this._onHotSpotDeSelected, this));
+        },
 
-            //********************************************************************************
-            //*PRIVATE OBJECT METHODS ********************************************************
-            //********************************************************************************
+        //********************************************************************************
 
-            /**
-            * Attach events here
-            *
+        /**
+         * Recreates the product selector with an initial value so that we don't trigger
+         * the change event
+         *
+         * @param value
+         * @private
+         */
+        _createProductSelector: function (value) {
+            $(this.linkToProduct).empty();
+
+            this.productSelector = new ContentSelector({
+                value: value,
+                cultureSpecific: false,
+                allowedTypes: this.allowedTypes != null ? this.allowedTypes : ["episerver.core.icontentdata"],
+                roots: this.roots != null ? this.roots : ["-1073741823__CatalogContent"],
+                label: this._resources.content,
+                allowedDndTypes: this.allowedDndTypes != null ? this.allowedDndTypes : ["episerver.core.icontentdata.reference"]
+            });
+
+            $(this.linkToProduct).append(this.productSelector.domNode);
+
+            this.productSelector.on('change', jQuery.proxy(this._onLinkToProductChange, this));
+        },
+
+        //********************************************************************************
+
+        /**
+            * Called on init to load the value from database and set on the control
+            * @param value
             * @private
             */
-            _attachEventListeners: function () {
-                jQuery(this.HotSpotsEditor).on('change', jQuery.proxy(this._onChange, this));
-                jQuery(this.HotSpotsEditor).on('select', jQuery.proxy(this._onHotSpotSelected, this));
-                jQuery(this.HotSpotsEditor).on('deselect', jQuery.proxy(this._onHotSpotDeSelected, this));
+        _setValueAttr: function (value) {
+            this.hotspots = value;
+            this._set('value', value);
 
-            },
+            if (!this.HotSpotsEditor) return;
+            this.HotSpotsEditor.setValue(value);
+        },
 
-            //********************************************************************************
+        //********************************************************************************
+        //*EVENT METHODS******************************************************************
+        //********************************************************************************
 
-		    /**
-			 * Recreates the product selector with an initial value so that we don't trigger
-			 * the change event
-			 *
-			 * @param value
-			 * @private
-			 */
-            _createProductSelector: function (value) {
-
-                $(this.linkToProduct).empty();
-
-                this.productSelector = new ContentSelector({
-                    value: value,
-                    cultureSpecific: false,
-                    allowedTypes: ["episerver.core.icontentdata"],
-                    roots: ["-1073741823__CatalogContent"],
-                    label: 'Select product',
-                    allowedDndTypes: ["episerver.core.icontentdata.reference"]
-                });
-
-                $(this.linkToProduct).append(this.productSelector.domNode);
-
-                this.productSelector.on('change', jQuery.proxy(this._onLinkToProductChange, this));
-            },
-
-            //********************************************************************************
-
-            /**
-	            * Called on init to load the value from database and set on the control
-	            * @param value
-	            * @private
-	            */
-            _setValueAttr: function (value) {
-                this.hotspots = value;
+        /**
+        * When any HotSpots are modified, save the changes
+        * @param event
+        * @private
+        */
+        _onChange: function (event) {
+            var value = event.target.getValue();
+            if (this.value !== value) {
+                // this will trigger the save event on object, giving the option to publish the content
+                this.parent.editing = true;
                 this._set('value', value);
+                this.onChange(value);
 
-                if (!this.HotSpotsEditor) return;
-                this.HotSpotsEditor.setValue(value);
-            },
-
-            //********************************************************************************
-            //*EVENT METHODS******************************************************************
-            //********************************************************************************
-
-            /**
-            * When any HotSpots are modified, save the changes
-            * @param event
-            * @private
-            */
-            _onChange: function (event) {
-                var value = event.target.getValue();
-                //window.console.log('change event: ', this.value !== value, this.value, value);
-                if (this.value !== value) {
-
-                    // this will trigger the save event on object, giving the option to publish the content
-                    this.parent.editing = true;
-                    this._set('value', value);
-                    this.onChange(value);
-
-                    this.parent.editing = false;
-                }
-            },
-
-            //********************************************************************************
-
-		    /**
-			 * Updates the value of the linkToProduct control when a HotSpot is selected
-			 *
-			 * @param event
-			 * @param hotSpot
-			 * @private
-			 */
-            _onHotSpotSelected: function (event, hotSpot) {
-
-                this.activeHotSpot = hotSpot;
-
-                var link = this.activeHotSpot.getLink();
-
-                this._createProductSelector(link);
-
-            },
-
-            //********************************************************************************
-
-		    /**
-			 *
-			 * @param event
-			 * @private
-			 */
-            _onHotSpotDeSelected: function (event) {
-                this.activeHotSpot = undefined;
-            },
-
-            //********************************************************************************
-
-		    /**
-			 * When a product is selected or deselected update the product link on the HotSpot
-			 *
-			 * @param event
-			 * @private
-			 */
-            _onLinkToProductChange: function (event) {
-
-                if (this.activeHotSpot) {
-                    this.activeHotSpot.setLink(this.productSelector.value);
-                    this.activeHotSpot.saveChanges();
-                }
-
+                this.parent.editing = false;
             }
+        },
 
+        //********************************************************************************
 
-        });
+        /**
+         * Updates the value of the linkToProduct control when a HotSpot is selected
+         *
+         * @param event
+         * @param hotSpot
+         * @private
+         */
+        _onHotSpotSelected: function (event, hotSpot) {
+            this.activeHotSpot = hotSpot;
+
+            var link = this.activeHotSpot.getLink();
+
+            this._createProductSelector(link);
+        },
+
+        //********************************************************************************
+
+        /**
+         *
+         * @param event
+         * @private
+         */
+        _onHotSpotDeSelected: function (event) {
+            this.activeHotSpot = undefined;
+        },
+
+        //********************************************************************************
+
+        /**
+         * When a product is selected or deselected update the product link on the HotSpot
+         *
+         * @param event
+         * @private
+         */
+        _onLinkToProductChange: function (event) {
+            if (this.activeHotSpot) {
+                this.activeHotSpot.setLink(this.productSelector.value);
+                this.activeHotSpot.saveChanges();
+            }
+        }
+    });
 });
--- src/Geta.EPi.HotspotsEditor/Cms/Models/HotSpotContainer.cs
+++ src/Geta.EPi.HotspotsEditor/Cms/Models/HotSpotContainer.cs
@@ -1,6 +1,4 @@
-using System.Web.UI.WebControls;
 using EPiServer;
-using EPiServer.Commerce.Catalog.ContentTypes;
 using EPiServer.Core;
 using EPiServer.ServiceLocation;
 using Newtonsoft.Json;
@@ -13,7 +11,7 @@ namespace Geta.EPi.HotspotsEditor.Cms.Models
         private IContent _content;
 
         public HotSpotContainer() : this(ServiceLocator.Current.GetInstance<IContentLoader>())
-        {}
+        { }
 
         public HotSpotContainer(IContentLoader contentLoader)
         {
@@ -22,57 +20,42 @@ namespace Geta.EPi.HotspotsEditor.Cms.Models
 
         public string Link { get; set; }
 
-        private void LoadContent()
-        {
-            if (!string.IsNullOrEmpty(Link) && _content == null)
-            {
-                ContentReference.TryParse(Link, out ContentReference contentReference);
-                _contentLoader.TryGet(contentReference, out _content);
-            }
-        }
-
         [JsonIgnore]
-        public EntryContentBase IndexedProduct
-        {
-            get
-            {                
-                ContentReference.TryParse(Link, out ContentReference contentReference);
-                _contentLoader.TryGet(contentReference, out EntryContentBase entryBase);
-
-                return entryBase;
-            }
-        }
-
-        [JsonIgnore]
-        public string ProductCode
+        public IContent Content
         {
             get
             {
-                ContentReference.TryParse(Link, out ContentReference contentReference);
-                _contentLoader.TryGet(contentReference, out EntryContentBase product);
-
-                if (product == null) return string.Empty;
-
-                return product.Code;
+                if (_content == null && !string.IsNullOrEmpty(Link) && _content == null && ContentReference.TryParse(Link, out ContentReference contentReference))
+                {
+                    _contentLoader.TryGet(contentReference, out _content);
+                }
+                return _content;
             }
         }
 
         public Rectangle HotSpot { get; set; }
+
         public FRectangle Area { get; set; }
 
         public class Rectangle
         {
             public decimal Top { get; set; }
+
             public decimal Left { get; set; }
+
             public int Width { get; set; }
+
             public int Height { get; set; }
         }
 
         public class FRectangle
         {
             public decimal Top { get; set; }
+
             public decimal Left { get; set; }
+
             public decimal Width { get; set; }
+
             public decimal Height { get; set; }
         }
     }
